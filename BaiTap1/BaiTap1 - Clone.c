fd8e66b53aa531534a09d174983c5eba897b7d8e
DuongTung <duongdoanbach@gmail.com>
2022-03-28 10:33:01 +0700

    Added bai 6,7

--- BaiTap1/BaiTap1 - Clone.c
+++ BaiTap1/BaiTap1 - Clone.c
@@ -6,10 +6,48 @@
 #include <string.h>
 #include <conio.h>
 #include <stdbool.h>
-#include "commonmath.h"
 #pragma warning(disable:4996)
 #pragma warning(disable:6031)
+bool isPrime(int n) {
+	
+	for (int i = 2; i <= sqrt(n); i++)
+	{
+		if (n % i == 0)return true;
+	}
+	return false;
+};
+//find the largest value in a list and return it 
+int findmax(int *p,int n) {
 
+	int max = *p;
+	int* p_ = p;
+	for (int i = 0; i < n; i++)
+	{
+		max = *p_ > max ? *p_ : max;
+		p_++;
+	}
+	return max;
+};
+//find the smallest value in a list and return it 
+int findmin(int* p,int n) {
+	int min = *p;
+	int* p_ = p;
+	for (int i = 0; i < n; i++)
+	{
+		min = *p_ < min ? *p_ : min;
+		p_++;
+	}
+	return min;
+};
+//create a function that take a list of numbers and return the average of the numbers
+double findaverage(int* p[], int n) {
+	double sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += *p[i];
+	}
+	return sum / n;
+};
 void Bai2() {
 	int a, b, a_, b_;
 	printf("Nhap a:");
